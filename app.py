c0d1b48abc3e456fd88c1e9f0eee37ae46dbd481
tmtnguyen23 <tmtnguyen@unomaha.edu>
2025-02-25 19:18:35 -0600

    Small fix to the export file

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ def index():
     # Render the base.html template
     return render_template("base.html")
 
-'''
+# TODO: write json out to a page 
 @app.route("/regression")
 def linear_regression_output():
+
     with open('data/CS_predictions.json', 'r') as f:
         CS_predictions = json.load(f)
-        return json.dump(CS_predictions, f, indent=4)
-'''
+        print(CS_predictions)
+     
+    return json.dump(CS_predictions, f, indent=4)
 
-# Add more routes here once we get there
+
+# TODO: once it's done, remove the debug mode
 if __name__ == '__main__':
-    app.run() 
+    app.run(debug=True) 
