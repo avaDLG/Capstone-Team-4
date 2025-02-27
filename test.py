74189c06cf0f6b25e9a4227b4db4a9708ffbf973
avaDLG <100103395+avaDLG@users.noreply.github.com>
2025-02-27 13:53:19 -0600

    deleted test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import json 
-
-with open('data/cs_enrollment.json', 'r') as f:
-        cs_predictions = json.load(f)
-        for subject, courses in cs_predictions.items():
-            print(courses)
