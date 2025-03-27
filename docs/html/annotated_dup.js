ef1646e07a92338263155dd70992bd797256f340
avaDLG <100103395+avaDLG@users.noreply.github.com>
2025-03-27 16:54:44 -0500

    doxygen html page working
    '

---
+++ docs/html/annotated_dup.js
@@ -0,0 +1,8 @@
+var annotated_dup =
+[
+    [ "app", null, [
+      [ "LoginForm", "classapp_1_1_login_form.html", null ],
+      [ "RegistrationForm", "classapp_1_1_registration_form.html", null ],
+      [ "User", "classapp_1_1_user.html", null ]
+    ] ]
+];
---
+++ docs/html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['registrationform_0',['RegistrationForm',['../classapp_1_1_registration_form.html',1,'app']]]
+];
